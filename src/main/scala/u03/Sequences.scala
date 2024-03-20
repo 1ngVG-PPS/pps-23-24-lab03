59225f27ffc9bb4b22dd9fa352a30bba5e7576d0
Valerio-Giannini <valerio.giannini@studio.unibo.it>
2024-03-20 21:40:27 +0100

    task 1: take and zip

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -25,11 +25,15 @@ object Sequences: // Essentially, generic linkedlists
       case Nil()                 => Nil()
 
     // Lab 03
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = 
+      Cons((h: A , h: B), zip(t: A, t: B))
 
-    def take[A](l: Sequence[A])(n: Int): Sequence[A] = ???
+    def take[A](l: Sequence[A])(n: Int): Sequence[A] = l match
+      case Nil() => Nil()
+      case Cons(h, t) if n > 0 => Cons (h, take(t)(n - 1)) 
     
     def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = ???
+
     def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
 
     def min(l: Sequence[Int]): Optional[Int] = ???
